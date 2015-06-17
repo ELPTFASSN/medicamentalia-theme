61120b548874e702b5e23b8b5da0d24fea80e7e4
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-06-17 10:55:40 +0200

    Fix styles based on translations

--- templates/home-item.php
+++ templates/home-item.php
@@ -1,4 +1,5 @@
-<a class="portfolio-box" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+<?php $origin_post_ID = icl_object_id(get_the_ID(), 'page', false, 'es'); ?>
+<a class="portfolio-box portfolio-id-<?= $origin_post_ID; ?>" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
   <?php $bkg = wp_get_attachment_url( get_post_thumbnail_id(), 'large' ); ?>
   <?php if( $bkg ) : ?>
   <div class="portfolio-box-background" style="background-image: url('<?php echo $bkg; ?>')"></div>
@@ -10,7 +11,11 @@
     <div class="portfolio-box-caption-content">
       <h3><?php the_title(); ?></h3>
       <div class="portfolio-box-caption-description"><?php the_field('home_description'); ?></div>
-      <button class="read-more-btn btn">Leer Más</button>
+      <?php if( $origin_post_ID != 2 ) :  ?>
+      <button class="read-more-btn btn"><?= __('Leer Más', 'sage'); ?></button>
+      <?php else: ?>
+      <button class="read-more-btn btn"><?= __('Explora los datos', 'sage'); ?></button>
+      <?php endif; ?>
     </div>
   </div>
 </a>
