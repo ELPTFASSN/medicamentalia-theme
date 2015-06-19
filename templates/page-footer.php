66c81f8ef761aa358de29dffa114c7beae6d5c52
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-06-19 20:35:21 +0200

    Fix related content on page footer

--- templates/page-footer.php
+++ templates/page-footer.php
@@ -3,7 +3,8 @@
     <div class="container-fluid">
       <div class="row no-gutter">
         <?php $ids = array(2,8,10,12); ?>
-        <?php $ids = array_diff($ids, array(get_the_ID())); // remove current page id ?>
+        <?php $originID = icl_object_id(get_the_ID(), 'page', false, 'es'); ?>
+        <?php $ids = array_diff($ids, array($originID)); // remove current page id ?>
         <?php $home_query = new WP_Query( array('post_type' => 'page', 'post__in' => $ids) ); ?>
         <?php  while ( $home_query->have_posts() ) : $home_query->the_post(); ?>
           <div <?php post_class('col-md-4'); ?>>
